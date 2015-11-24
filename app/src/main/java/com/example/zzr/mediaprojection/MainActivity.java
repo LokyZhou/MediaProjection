de8a5976f1d06c935785d3e732e4fbbfc4296ab2
LokyZhou <zhou19910626@gmail.com>
2015-11-24 18:39:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zzr/mediaprojection/MainActivity.java
@@ -0,0 +1,199 @@
+package com.example.zzr.mediaprojection;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+    private static final int PERMISSION_CODE = 1;
+    private int mScreenDensity;
+    private MediaRecorder mMediaRecorder;
+    private MediaProjectionManager mProjectionManager;
+    private ToggleButton mToggleButton;
+    private MediaProjectionCallback mMediaProjectionCallback;
+    private MediaProjection mMediaProjection;
+    private static final String TAG = "MediaProjection_test";
+    private VirtualDisplay mVirtualDisplay;
+    private static final int DISPLAY_WIDTH = 1080;
+    private static final int DISPLAY_HEIGHT = 1920;
+    private int buffersize = 500000;
+    public MainActivity() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity = metrics.densityDpi;
+        mMediaRecorder = new MediaRecorder();
+        initRecorder();
+        prepareRecorder();
+        mProjectionManager = (MediaProjectionManager)getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        mToggleButton = (ToggleButton)findViewById(R.id.toggleButton);
+        mToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onToggleScreenShare(v);
+            }
+        });
+        mMediaProjectionCallback=new MediaProjectionCallback();
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if(mMediaProjection != null){
+            mMediaProjection.stop();
+            mMediaProjection = null;
+        }
+    }
+
+    public void onToggleScreenShare(View view)
+    {
+        if(((ToggleButton) view).isChecked())
+        {
+            shareScreen();
+        } else {
+        mMediaRecorder.stop();
+        mMediaRecorder.reset();
+        mMediaProjection.stop();
+        Log.v(TAG, "Recording Stopped");
+        stopScreenSharing();
+        initRecorder();
+        prepareRecorder();
+        }
+    }
+
+    public void stopScreenSharing() {
+        if (mVirtualDisplay == null) {
+            return;
+        }
+        mVirtualDisplay.release();
+    }
+
+    public void shareScreen() {
+        if(mMediaProjection == null)
+        {
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(),PERMISSION_CODE);
+            return;
+        }
+        mVirtualDisplay = createVirtualDisplay();
+        mMediaRecorder.start();
+
+    }
+
+    private VirtualDisplay createVirtualDisplay() {
+        return mMediaProjection.createVirtualDisplay("MainActivity",
+                DISPLAY_WIDTH, DISPLAY_HEIGHT, mScreenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mMediaRecorder.getSurface(), null /*Callbacks*/, null /*Handler*/);
+    }
+
+
+    public void prepareRecorder() {
+        try {
+            mMediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+            finish();
+        }catch(IllegalStateException e)
+        {
+            e.printStackTrace();
+            finish();
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != PERMISSION_CODE) {
+            Log.e(TAG, "Unknown request code: " + requestCode);
+            return;
+        }
+        if (resultCode != RESULT_OK) {
+            Toast.makeText(this,
+                    "Screen Cast Permission Denied", Toast.LENGTH_SHORT).show();
+            mToggleButton.setChecked(false);
+            return;
+        }
+        mMediaProjection = mProjectionManager.getMediaProjection(resultCode, data);
+        mMediaProjection.registerCallback(mMediaProjectionCallback, null);
+        mVirtualDisplay = createVirtualDisplay();
+        mMediaRecorder.start();
+    }
+
+    public void initRecorder() {
+
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mMediaRecorder.setVideoEncodingBitRate(10000000);
+   //     mMediaRecorder.setVideoFrameRate(30);
+        mMediaRecorder.setVideoSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+        mMediaRecorder.setOutputFile("/sdcard/DCIM/Camera/xiaokaxiu/capture.mp4");
+   //     mMediaRecorder.setOutputFile();后期用来设置流
+        mMediaRecorder.setVideoFrameRate(60);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public class MediaProjectionCallback extends MediaProjection.Callback {
+        @Override
+        public void onStop() {
+            if (mToggleButton.isChecked()) {
+                mToggleButton.setChecked(false);
+                mMediaRecorder.stop();
+                mMediaRecorder.reset();
+                Log.v(TAG, "Recording Stopped");
+                initRecorder();
+                prepareRecorder();
+            }
+            mMediaProjection = null;
+            stopScreenSharing();
+            Log.i(TAG, "MediaProjection Stopped");
+        }
+    }
+
+}
